d9a8d15e6b4b9ee30d081b6aa8b5dda0640746a2
Gergely Daroczi <gergely.daroczi@card.com>
2016-10-22 03:45:04 +0200

    improve docs by references, example and cross references

--- R/kms.R
+++ R/kms.R
@@ -1,11 +1,13 @@
 #' Encrypt plain text via KMS
-#' @param key KMS key ID, fully specified Amazon Resource Name or an alias with the \code{alias/} prefix
-#' @param text max 4096 bytes long string
-#' @return base64-encoded text
+#' @param key the KMS customer master key identifier as a fully specified Amazon Resource Name (eg \code{arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012}) or an alias with the \code{alias/} prefix (eg \code{alias/foobar})
+#' @param text max 4096 bytes long character vector, eg an RSA key, a database password, or other sensitive customer information
+#' @return Base64-encoded text
 #' @export
+#' @references \url{http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/kms/AWSKMSClient.html#encrypt-com.amazonaws.services.kms.model.EncryptRequest-}
 #' @examples \dontrun{
 #' kms_encrypt('alias/mykey', 'foobar')
 #' }
+#' @seealso kms_decrypt
 kms_encrypt <- function(key, text) {
 
     ## prepare the request
@@ -24,9 +26,11 @@ kms_encrypt <- function(key, text) {
 
 
 #' Decrypt cipher into plain text via KMS
-#' @param cipher base64-encoded ciphertext
-#' @return string
+#' @param cipher Base64-encoded ciphertext
+#' @return decrypted text
 #' @export
+#' @references \url{http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/kms/AWSKMSClient.html#decrypt-com.amazonaws.services.kms.model.DecryptRequest-}
+#' @seealso kms_encrypt
 kms_decrypt <- function(cipher) {
 
     ## prepare the request
